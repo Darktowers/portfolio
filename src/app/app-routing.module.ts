f0ea54da9f283c869528609f4db9d8b342936644
Cristian Arrieta <darktowerdev@gmail.com>
2020-06-02 21:58:12 -0500

    My Work and minor adjusts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./pages/home/home/home.module').then(m => m.HomeModule),
   },
   {
     path: 'about',
     loadChildren: () => import('./pages/about/about/about.module').then(m => m.AboutModule),
+  },
+  {
+    path: 'my-work',
+    loadChildren: () => import('./pages//work/work/work.module').then(m => m.WorkModule),
   },
 ];
 
